327a10bc8b3be7b6dc9788f0e16364debb4860bd
frontendry <brngatarwa@gmail.com>
2021-10-07 21:31:42 +0300

    api update

--- src/actions/countriesActions.js
+++ src/actions/countriesActions.js
@@ -3,6 +3,14 @@ export const SEARCH_COUNTRIES = "GET_COUNTRIES";
 export const SEARCH_COUNTRIES_SUCCESS = "GET_COUNTRIES_SUCCESS";
 export const SEARCH_COUNTRIES_FAILURE = "GET_COUNTRIES_FAILURE";
 
+const fetchOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+    "x-rapidapi-key": "6365ac3d4dmsh02f138d221ee4d7p1deccajsnf02f93c75af2",
+  },
+};
+
 // Countries Action Creators
 export const searchCountries = () => ({
   type: SEARCH_COUNTRIES,
@@ -23,15 +31,23 @@ export function fetchCountries(name) {
     dispatch(searchCountries());
 
     try {
-      const response = await fetch(
+      /* const response = await fetch(
         `https://api.countrylayer.com/v2/name/${name}?access_key=${process.env.REACT_APP_COUNTRIES_API}`
+      ); */
+
+      const response = await fetch(
+        `https://restcountries-v1.p.rapidapi.com/name/${name}`,
+        fetchOptions
       );
 
       const data = await response.json();
 
+      console.log(response);
+      console.log(data);
+
       dispatch(searchCountriesSuccess(data));
     } catch (error) {
-      console.log(error);
+      console.error(error);
       dispatch(searchCountriesFailure());
     }
   };
